668549390eda16753a2bd8007af31c5f0873caf8
okromelidzegiorgi7 <okromelidzegiorgi7@gmail.com>
2025-11-14 09:42:41 -0800

    second commit

---
+++ dey6/homework/hw3.py
@@ -0,0 +1,13 @@
+ # 3)ჩამოწერე ყველა შედარების ოპერატორი მაგალით
+
+a = 30
+b = 20
+
+print(a == b)
+print(a != b)
+
+print(a>b)
+print(a<b)
+
+print(a>=b)
+print(a<=b)
---
+++ dey6/homework/hw4.py
@@ -0,0 +1,10 @@
+# 4)შექმენით 2 ცვლადი სადაც შეეკითხებოდით რენდომ რიცხვებს,შემდეგ კი შეადარებთ ერთმანეთს
+
+import random
+
+a = random.randint(1, 100)
+b = random.randint(2, 300)
+
+print("a =", a)
+print("b =", b)
+print("a > b:", a > b)
