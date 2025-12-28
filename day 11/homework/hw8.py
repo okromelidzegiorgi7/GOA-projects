ec80d594113cb0381a7f399ed3a26cea775041a9
okromelidzegiorgi7 <okromelidzegiorgi7@gmail.com>
2025-12-28 06:15:29 -0800

    second commit

---
+++ day 11/homework/hw8.py
@@ -0,0 +1,13 @@
+
+
+#<!DOCTYPE html>
+#<html lang="ka">
+#<head>
+#    <meta charset="UTF-8">
+#    <title>ძაღლები</title>
+#</head>
+#<body>
+#    <h1>ძაღლები</h1>
+ #   <p>ძაღლები მეგობრული შინაური ცხოველები არიან.</p>
+#</body>
+#</html>
