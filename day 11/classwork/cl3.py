3e2b3a37de7f9f7f17ea0930a47b2074b4780924
okromelidzegiorgi7 <okromelidzegiorgi7@gmail.com>
2026-01-11 18:08:47 -0800

    second commit

---
+++ day 11/classwork/cl3.py
@@ -0,0 +1,8 @@
+#3)მომხარებელს შემოატანინე სახელი, თუ ეს სახელი უდრის aleksandre ს, დაპრინტეთ "mentor" სხვა შემთხვევაში ამ ასოში თითოეული ასო ცალ ცალკე გამოიტანეთ ფორ ლუპებით
+#ბატონი ალექსანდრე ძუკაევი — 8:21 AM
+
+name = input("Enter your name")
+if name == "aleksandre":
+    print("mentor")
+for letter in name:
+        print(letter)
---
+++ day 12/homework/hw2.py
@@ -0,0 +1,12 @@
+#2) როგორ შეგვიძლია უცებ  შევქმნათ html ის სტრუქტურა
+
+#<!DOCTYPE html>
+#<html lang="en">
+#<head>
+#    <meta charset="UTF-8">
+#   <title>Document</title>
+#</head>
+#<body>
+
+#</body>
+#</html>
