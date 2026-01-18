c7a09b463fcbcb53fcfabec4c39e4f0ba98166dc
okromelidzegiorgi7 <okromelidzegiorgi7@gmail.com>
2026-01-18 14:07:42 -0800

    second commit

---
+++ day 15/homework/hw 5.py
@@ -0,0 +1,22 @@
+#5)sololearn using indexing ებამდე
+
+my_list = [10, "hello", 3.5, True, "Python", 99, "a", "b", "c", "d"]
+
+print(my_list[0])
+print(my_list[4])
+
+print(my_list[-1])
+print(my_list[-2])
+
+my_list[1] = "new element"
+print(my_list)
+
+print(len(my_list))
+
+print(my_list[len(my_list) - 1])
+
+print(my_list[-len(my_list)])
+print(my_list[-1])
+
+print(my_list[:3])
+print(my_list[-3:])
