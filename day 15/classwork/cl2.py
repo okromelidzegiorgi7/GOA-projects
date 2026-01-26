6b85ff2d4ce35bb851cddeb1852093fa3713c977
okromelidzegiorgi7 <okromelidzegiorgi7@gmail.com>
2026-01-25 18:29:03 -0800

    second commit

---
+++ day 15/classwork/cl2.py
@@ -0,0 +1,20 @@
+#2)შექმენით საიტი,  2 პარაგრაფით  და გამოიყენეთ text formating თეგები  ყველა
+
+#<!DOCTYPE html>
+#<html lang="ka">
+#<head>
+#    <meta charset="UTF-8">
+#    <title>Text Formatting მაგალითი</title>
+#</head>
+#<body>
+
+#<p>
+#    ეს არის <b>მუქი ტექსტი</b>, <strong>მნიშვნელოვანი ტექსტი</strong>, <i>დახრილი ტექსტი</i>, <em>ხაზგასმული მნიშვნელობით</em>, <u>ხაზგასმული ტექსტი</u> და <mark>მონიშნული ტექსტი</mark>.
+#</p>
+
+#<p>
+#    <small>პატარა ტექსტი</small>, H<sub>2</sub>O (ქვედა ინდექსი), x<sup>2</sup> (ზედა ინდექსი), <del>წაშლილი ტექსტი</del>, <ins>დამატებული ტექსტი</ins>, <code>print("Hello")</code>.
+#</p>
+
+#</body>
+#</html>
---
+++ day 15/classwork/cl3.py
@@ -0,0 +1,30 @@
+
+
+#<doctype html>
+#<html lang="ka">
+#<head>
+#    <meta charset=uTF-8>
+#    <title>Home Page</title>
+#</head>
+#<body>
+
+#<h1>Home Page</h1>
+
+#<!-- მენიუ გვერდებზე გადასასვლელად -->
+#<p>
+#    <a href="home.html">Home</a> |
+#    <a href="about.html">About Us</a> |
+#    <a href="contact.html">Contact Us</a>
+#</p>
+
+#<p>
+#    ეს არის <b>მუქი ტექსტი</b> და <i>დახრილი ტექსტი</i>.
+ #0   ასევე <u>ხაზგასმული ტექსტი</u> და <mark>მონიშნული ტექსტი</mark>.
+#</p>
+
+#<p>
+#    <small>პატარა ტექსტი</small>, H<sub>2</sub>O, x<sup>2</sup>, <del>წაშლილი ტექსტი</del>, <ins>დამატებული ტექსტი</ins>, <code>print("Hello")</code>.
+#</p>
+
+#</body>
+#</html>
