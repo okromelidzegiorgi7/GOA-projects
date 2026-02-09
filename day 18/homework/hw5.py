d50725132f2390c2b7b796244ddc8ba6c79e9490
okromelidzegiorgi7 <okromelidzegiorgi7@gmail.com>
2026-02-08 18:38:14 -0800

    second commit

--- day 18/homework/hw5.py
+++
@@ -1,6 +0,0 @@
-#days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-#ამოიღე ელემენტები 'tue'-დან 'fri'-მდე (ჩათვლით).
-
-days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-result = days [2:6]
-print(result)
